c1d79cddc96613650fa1f17e47d405f16f4e7b77
Zahidbhai Dodiya <zahidbhai.dodiya@ad.infosys.com>
2022-01-03 22:03:14 +0530

    Commiting changes

---
+++ ZoosProblem/ZoosProblem/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ZoosProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string[] inputChars;
+
+            //Console.WriteLine("Enter the word : ");
+            input = Console.ReadLine();
+            inputChars = new string[input.Length];
+
+            for (int i = 0; i <= input.Length - 1; i++)
+            {
+                inputChars[i] = input[i] + "";
+            }
+
+            int countZ = 0;
+            int countO = 0;
+
+            for (int i = 0; i <= input.Length - 1; i++)
+            {
+                if(inputChars[i].Equals("z"))
+                {
+                    countZ++;
+                }
+                else if (inputChars[i].Equals("o"))
+                {
+                    countO++;
+                }
+            }
+
+            if(countO == (countZ * 2))
+            {
+                Console.WriteLine("Yes");//Considered as Zoo
+            }
+            else
+            {
+                Console.WriteLine("No");//Not considered as Zoo
+            }
+        }
+    }
+}
