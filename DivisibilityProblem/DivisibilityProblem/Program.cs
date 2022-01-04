d4fa7890c0ba0c2802a0a2f8a040f5f8f2f1ccb5
Zahidbhai Dodiya <zahidbhai.dodiya@ad.infosys.com>
2022-01-04 17:19:28 +0530

    commiting problem

---
+++ DivisibilityProblem/DivisibilityProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DivisibilityProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int[] array;
+
+            Console.WriteLine("Enter number of numbers : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            array = new int[num];
+
+
+            Console.WriteLine("Enter numbers : ");
+            string[] temp = Console.ReadLine().Split(" ");
+            if (temp[num - 1].ToString().EndsWith("0"))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
