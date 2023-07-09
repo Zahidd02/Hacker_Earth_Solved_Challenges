cc573ca7585bfa48bd7226a2bd1787a602387c41
Zahid Dodiya <96968285+Zahidd02@users.noreply.github.com>
2023-07-09 12:22:41 +0530

    New Solution - 09 July 2023

---
+++ Codestudio/Disappearing Pattern/main.py
@@ -0,0 +1,22 @@
+letters = "ABCDEFG"
+arr = []
+
+# Time Complexity : O(n3)
+for x in range(len(letters) - 2, -1, -1):  # Adds the full (except last element) string in reverse to original string.
+    letters += letters[x]
+
+for i in range(0, len(letters)):  # [6], [5, 6, 7], [4, 5, 6, 7, 8], ...
+    for j in range(0, len(letters)):
+        if not arr.__contains__(j):
+            print(letters[j], end="")
+        else:
+            print(" ", end="")
+    if len(arr) == 0:
+        arr.append(len(letters) // 2)
+    else:
+        arr.append(arr[len(arr) - 1] + 1)
+        arr.append(arr[0]-1)
+        arr.sort()
+        if len(arr) == len(letters):
+            break
+    print("")
